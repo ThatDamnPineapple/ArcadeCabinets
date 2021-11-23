75d39cdcbb4064d64b119809585b9569b156b056
GraydeeSi <wiseguy0002@gmail.com>
2021-11-23 16:20:01 -0500

    Added more gameobject properties (will add more as I think of them)

--- Interfaces/IGameObject.cs
+++ Interfaces/IGameObject.cs
@@ -8,6 +8,12 @@ namespace ArcadeCabinets.Interfaces {
     {
         Vector2 Position { get; set; }
 
+        Texture2D Texture { get; set; }
+
+        Rectangle Frame { get; set; }
+
+        Rectangle Hitbox { get; set; }
+
         void Draw(SpriteBatch spriteBatch);
 
         void Update();
